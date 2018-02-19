3f5a8301105faf2b89471ad9b4cd21b37747738e
moystre <moystre@gmail.com>
2018-02-19 11:03:32 +0100

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
+import { ProfileComponent } from './user/profile/profile.component';
+import { FileSystemComponent } from './home/file-system/file-system.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+
+const routes: Routes = [
+    {
+      path: 'login', 
+      component: LoginComponent 
+    },
+    {
+      path: 'home',
+      component: FileSystemComponent
+    },
+    {
+      path: 'user',
+      component: ProfileComponent
+    }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
