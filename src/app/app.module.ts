9a73a27defb88c7e351ffef7baa72fa66452732b
moystre <moystre@gmail.com>
2018-02-19 10:46:10 +0100

    adding auth/login, home/file-system, user/profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileSystemComponent } from './file-system/file-system.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FileSystemComponent]
 })
 export class HomeModule { }
